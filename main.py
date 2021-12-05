40bcbf81b1759066e7af93795d85c22c76843963
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2021-12-05 17:14:45 +0100

    using crossEncoder

--- main.py
+++ main.py
@@ -17,14 +17,9 @@ with open('./data/dev_data.csv', newline='', encoding="utf8") as csvfile:
                 print(row)
             else:
                 #TODO: verwijderen als we volledige dataset inlezen
-                if temp < 100:
+                if temp < 1000:
                     temp += 1
                     dev_data_dict.append(row)
-
-
-#Define the model. Either from scratch of by loading a pre-trained model (pretrained here)
-# Note that this is a SentenceTransformer, not a crossencoder (which is what we eventually want)
-model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
 #Define your train examples. (Trains on first 100 rows of dataset)
 train_examples = []
@@ -33,16 +28,12 @@ for row in dev_data_dict:
 
 #Define your train dataset, the dataloader and the train loss
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
-train_loss = losses.CosineSimilarityLoss(model)
-
-#Tune the model
-model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100,save_best_model=True,output_path="./output/modelTest")
 
 # Crossencoders are used to predict how well a query matches the result, which is what we need
-model = CrossEncoder('./output/modelTest')
+model = CrossEncoder('cross-encoder/ms-marco-TinyBERT-L-2')
 
 # Does this need more training?
-model.fit(train_dataloader=train_dataloader, epochs=1, warmup_steps=100,save_best_model=True,output_path="./output/modelTest2")
+model.fit(train_dataloader=train_dataloader, epochs=1, warmup_steps=100,save_best_model=True,output_path="./output/modelTest")
 scores = model.predict([["This is correct", "This is correct"],
                         ["This is not", "Gibberish"]])
 print(scores)
